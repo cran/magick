881a2c7e7e634b133516133a496827ff005f273f
Jeroen Ooms <jeroen@berkeley.edu>
2023-12-20 17:20:02 +0000

    version 2.8.2

--- R/utils.R
+++ R/utils.R
@@ -2,21 +2,6 @@
 #' @export
 magrittr::`%>%`
 
-read_path <- function(path){
-  image <- if(is.character(path)){
-    if(is_url(path)){
-      read_url(path)
-    } else {
-      read_file(path)
-    }
-  } else if(is.raw(path)){
-    path
-  } else {
-    stop("Parameter 'image' must be an image object, file path or raw vector with image data")
-  }
-  return(image)
-}
-
 is_url <- function(path){
   grepl("^https?://", path)
 }
@@ -42,32 +27,25 @@ replace_url <- function(path){
   }
 }
 
+# Uses file extension from HTTP content-type if available to help IM guess type.
 download_url <- function(url){
-  req <- curl::curl_fetch_memory(url)
-  if(req$status_code >= 400)
-    stop(sprintf("Failed to download %s (HTTP %d)", url, req$status))
-  headers <- curl::parse_headers_list(req$headers)
-  ctype <- headers[['content-type']]
-  matches <- match(ctype, mimetypes$type)
-  extension <- if(length(matches) && !is.na(matches) && !grepl("(text|octet)", ctype)){
-    sub("*.", ".", mimetypes$pattern[matches[1]], fixed = TRUE)
+  tmp <- tempfile(fileext = sub("\\?.*", "", basename(url)))
+  if(requireNamespace('curl', quietly = TRUE)){
+    h <- curl::new_handle()
+    curl::curl_download(url, tmp, handle = h)
+    headers <- curl::parse_headers_list(curl::handle_data(h)$headers)
+    ctype <- headers[['content-type']]
+    matches <- match(ctype, mimetypes$type)
+    if(length(matches) && !is.na(matches) && !grepl("(text|octet)", ctype)){
+      extension <- sub("*.", ".", mimetypes$pattern[matches[1]], fixed = TRUE)
+      outfile <- tempfile(fileext = extension)
+      file.rename(tmp, outfile)
+      return(outfile)
+    }
   } else {
-    sub("\\?.*", "", basename(url))
+    utils::download.file(url, tmp, quiet = TRUE) #Fallback for webR
   }
-  filename <- tempfile(fileext = extension)
-  writeBin(req$content, filename)
-  return(filename)
-}
-
-read_url <- function(path){
-  req <- curl::curl_fetch_memory(path)
-  if(req$status_code >= 400)
-    stop(sprintf("Failed to download %s (HTTP %d)", path, req$status))
-  return(req$content)
-}
-
-read_file <- function(path){
-  readBin(normalizePath(path, mustWork = TRUE), raw(), file.info(path)$size)
+  return(tmp)
 }
 
 assert_image <- function(image){
